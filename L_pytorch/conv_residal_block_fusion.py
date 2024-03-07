01c98156ad4ba33efdc5cb7b699459f461accc29
CloudChaser-lv <158336861@qq.com>
2024-03-07 19:01:42 +0800

    conv_residal_block_fusion ac

--- L_pytorch/conv_residal_block_fusion.py
+++ L_pytorch/conv_residal_block_fusion.py
@@ -17,10 +17,10 @@ x = torch.ones(1, in_channels, w, h)
 conv_2d = nn.Conv2d(in_channels, out_channels, kernel_size, padding="same")
 conv_2d_pointwise = nn.Conv2d(in_channels, out_channels, 1)
 result1 = conv_2d(x) + conv_2d_pointwise(x) + x
-print(result1)
+# print(result1)
 
 # 方法二：算子融合
-
+# 1.原生写法
 pointwise_to_conv_weight = F.pad(conv_2d_pointwise.weight, [1,1,1,1,0,0,0,0])
 conv_2d_for_pointwise = nn.Conv2d(in_channels, out_channels, kernel_size, padding="same")
 conv_2d_for_pointwise.weight = nn.Parameter(pointwise_to_conv_weight)
@@ -40,7 +40,17 @@ conv_2d_for_identity.bias = nn.Parameter(identity_to_conv_bias)
 
 result2 = conv_2d(x) + conv_2d_for_pointwise(x) + conv_2d_for_identity(x)
 
-print(result2)
+# print(result2)
 print(torch.all(torch.isclose(result1, result2)))
+
+# 2.融合算子
+conv_2d_for_fusion = nn.Conv2d(in_channels, out_channels, kernel_size, padding="same")
+conv_2d_for_fusion.weight = nn.Parameter(conv_2d.weight.data + conv_2d_for_pointwise.weight.data + conv_2d_for_identity.weight.data)
+conv_2d_for_fusion.bias = nn.Parameter(conv_2d.bias.data + conv_2d_for_pointwise.bias.data + conv_2d_for_identity.bias.data)
+
+result3 = conv_2d_for_fusion(x)
+print(torch.all(torch.isclose(result2, result3)))
+
+
 
 
